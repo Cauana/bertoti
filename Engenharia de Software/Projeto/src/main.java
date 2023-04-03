7feb0c3652729430f5e6e372c85ab3bdd420a137
Cauana Dias <77700346+Cauana@users.noreply.github.com>
2023-04-03 19:31:25 -0300

    classe main

---
+++ Engenharia de Software/Projeto/src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+
+    public static void main(String[] args){
+        Usuario usuario1 = new Usuario("João", "joao123", "senha123");
+        System.out.println(usuario1.getNome());
+        membros.adicionarMembroi
+    }
+}
