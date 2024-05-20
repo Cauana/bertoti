ebd788d8a28a8122da024614d4e6b294d4e745e3
Cauana <cauana.dias@hotmail.com>
2024-05-20 11:43:49 -0300

    Update LojaOnline.java

--- Padrões de Projetos de Sistemas/PadroesdeProjetodeSistemas/src/main/java/com/example/DesignPatterns/Facade/Pattern/LojaOnline.java
+++ Padrões de Projetos de Sistemas/PadroesdeProjetodeSistemas/src/main/java/com/example/DesignPatterns/Facade/Pattern/LojaOnline.java
@@ -2,14 +2,22 @@ package main.java.com.example.DesignPatterns.Facade.Pattern;
 
 class LojaOnline {
     private Estoque estoque;
+    private Fatura fatura;
 
     public LojaOnline() {
         this.estoque = new Estoque();
+        this.fatura = new Fatura();
+    }
+
+    public void gerarFatura(String produto, int quantidade, String cliente) {
+        fatura = new Fatura();
+        fatura.gerarFatura(produto, quantidade, cliente);
     }
 
     public void realizarPedido(String produto, int quantidade, String cliente) {
         if (estoque.verificarDisponibilidade(produto, quantidade)) {
             System.out.println("Pedido de " + quantidade + " unidades de " + produto + " para " + cliente + " realizado com sucesso.");
+            gerarFatura(produto, quantidade, cliente);
         } else {
             System.out.println("Pedido de " + quantidade + " unidades de " + produto + " para " + cliente + " não pôde ser realizado devido a estoque insuficiente.");
         }
